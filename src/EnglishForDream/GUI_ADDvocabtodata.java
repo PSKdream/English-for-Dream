b6335d02f6359fcd39dabc2240c12c22417c60a4
jrapbit <67047826+jrapbit@users.noreply.github.com>
2020-07-04 17:43:49 +0700

    deploy ADDvocabtodata funtion
    ez

--- src/EnglishForDream/GUI_ADDvocabtodata.java
+++ src/EnglishForDream/GUI_ADDvocabtodata.java
@@ -5,25 +5,25 @@
  */
 package EnglishForDream;
 
+import Database.Database;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import TranslateTTS.*;
 
-
- /**
+/**
  *
  * @author ptmna
  */
 public class GUI_ADDvocabtodata extends javax.swing.JFrame {
 
-   
     public GUI_ADDvocabtodata() {
-     
+
         initComponents();
         Toolkit toolkit = getToolkit();
         Dimension size = toolkit.getScreenSize();
-        setLocation(size.width/2-getWidth()/2,size.height/2-getHeight()/2);
+        setLocation(size.width / 2 - getWidth() / 2, size.height / 2 - getHeight() / 2);
     }
 
     /**
@@ -37,15 +37,15 @@ public class GUI_ADDvocabtodata extends javax.swing.JFrame {
 
         jPanel3 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
-        next = new javax.swing.JButton();
-        translate = new javax.swing.JButton();
-        translatedLabel = new javax.swing.JLabel();
+        addTextToData = new javax.swing.JButton();
         texttospeech = new javax.swing.JButton();
+        translate1 = new javax.swing.JButton();
         jPanel5 = new javax.swing.JPanel();
         back = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
-        addword = new javax.swing.JTextField();
-        addvocabtodata = new javax.swing.JCheckBox();
+        wordField = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        translatedArea = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
@@ -58,19 +58,13 @@ public class GUI_ADDvocabtodata extends javax.swing.JFrame {
         jPanel2.setForeground(new java.awt.Color(255, 153, 153));
         jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        next.setBackground(new java.awt.Color(255,153,153));
-        next.setIcon(new javax.swing.ImageIcon(getClass().getResource("/EnglishForDream/next.png"))); // NOI18N
-        next.setBorder(null);
-        jPanel2.add(next, new org.netbeans.lib.awtextra.AbsoluteConstraints(1020, 230, 110, 90));
-
-        translate.setIcon(new javax.swing.ImageIcon(getClass().getResource("/EnglishForDream/bttran.png"))); // NOI18N
-        translate.setBorder(null);
-        jPanel2.add(translate, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 120, 190, 160));
-
-        translatedLabel.setFont(new java.awt.Font("Tahoma", 0, 48)); // NOI18N
-        translatedLabel.setForeground(new java.awt.Color(255, 255, 255));
-        translatedLabel.setText("Answer");
-        jPanel2.add(translatedLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 0, 410, 120));
+        addTextToData.setBorder(null);
+        addTextToData.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                addTextToDataActionPerformed(evt);
+            }
+        });
+        jPanel2.add(addTextToData, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 40, 190, 160));
 
         texttospeech.setBackground(new java.awt.Color(255,153,153));
         texttospeech.setIcon(new javax.swing.ImageIcon(getClass().getResource("/EnglishForDream/speechbt.png"))); // NOI18N
@@ -88,9 +82,18 @@ public class GUI_ADDvocabtodata extends javax.swing.JFrame {
                 texttospeechActionPerformed(evt);
             }
         });
-        jPanel2.add(texttospeech, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 120, 190, 160));
+        jPanel2.add(texttospeech, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 40, 190, 160));
 
-        jPanel3.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 390, 1150, 330));
+        translate1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/EnglishForDream/bttran.png"))); // NOI18N
+        translate1.setBorder(null);
+        translate1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                translate1ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(translate1, new org.netbeans.lib.awtextra.AbsoluteConstraints(800, 40, 190, 160));
+
+        jPanel3.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 490, 1150, 230));
 
         jPanel5.setBackground(new java.awt.Color(255, 153, 153));
         jPanel5.setForeground(new java.awt.Color(255, 153, 153));
@@ -112,24 +115,25 @@ public class GUI_ADDvocabtodata extends javax.swing.JFrame {
 
         jPanel3.add(jPanel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1150, 80));
 
-        addword.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
-        addword.setForeground(new java.awt.Color(102, 51, 0));
-        addword.setText("Answer");
-        addword.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(79, 39, 0), 7, true));
-        addword.addActionListener(new java.awt.event.ActionListener() {
+        wordField.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        wordField.setForeground(new java.awt.Color(102, 51, 0));
+        wordField.setToolTipText("");
+        wordField.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(79, 39, 0), 7, true));
+        wordField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                addwordActionPerformed(evt);
+                wordFieldActionPerformed(evt);
             }
         });
-        jPanel3.add(addword, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 200, 430, 70));
+        jPanel3.add(wordField, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 200, 430, 70));
 
-        addvocabtodata.setText("Addvocabtodata");
-        addvocabtodata.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                addvocabtodataActionPerformed(evt);
-            }
-        });
-        jPanel3.add(addvocabtodata, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 280, 110, -1));
+        translatedArea.setEditable(false);
+        translatedArea.setColumns(20);
+        translatedArea.setFont(new java.awt.Font("Tahoma", 0, 30)); // NOI18N
+        translatedArea.setLineWrap(true);
+        translatedArea.setRows(5);
+        jScrollPane1.setViewportView(translatedArea);
+
+        jPanel3.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 320, 430, 140));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -146,45 +150,60 @@ public class GUI_ADDvocabtodata extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void texttospeechMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_texttospeechMouseEntered
-        setMouseEntered_Exited(texttospeech,"speechover");
+        setMouseEntered_Exited(texttospeech, "speechover");
     }//GEN-LAST:event_texttospeechMouseEntered
 
     private void texttospeechMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_texttospeechMouseExited
-        setMouseEntered_Exited(texttospeech,"speechbt");
+        setMouseEntered_Exited(texttospeech, "speechbt");
     }//GEN-LAST:event_texttospeechMouseExited
 
-    private void addvocabtodataActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addvocabtodataActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_addvocabtodataActionPerformed
-
     private void backActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backActionPerformed
-       setVisible(false);
+        setVisible(false);
         new GUI_Epg().setVisible(true);
     }//GEN-LAST:event_backActionPerformed
 
     private void texttospeechActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_texttospeechActionPerformed
-        // TODO add your handling code here:
+        TextToSpeech tts = new TextToSpeech();
+        tts.speak(this.wordField.getText());
     }//GEN-LAST:event_texttospeechActionPerformed
 
-    private void addwordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addwordActionPerformed
+    private void wordFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_wordFieldActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_addwordActionPerformed
-    public void setMouseEntered_Exited(JButton a,String Imagefile){
-        ImageIcon icon = new ImageIcon("src/EnglishForDream/"+Imagefile+".png");
+    }//GEN-LAST:event_wordFieldActionPerformed
+
+    private void addTextToDataActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addTextToDataActionPerformed
+        Database db = new Database("jdbc:sqlite:data.db");
+        Translate translator = new Translate("AIzaSyBmbhyWZW_ywsIDuHEyKvfn1dillOn1hz4");
+        db.connect();
+        db.insert.insertData(this.wordField.getText(), translator.translate(this.wordField.getText(), "en", "th"));
+        db.close();
+        this.translate1ActionPerformed(null);
+    }//GEN-LAST:event_addTextToDataActionPerformed
+
+    private void translate1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_translate1ActionPerformed
+
+        Translate translator = new Translate("AIzaSyBmbhyWZW_ywsIDuHEyKvfn1dillOn1hz4");
+        String text = translator.translate(this.wordField.getText(), "en", "th");
+        System.out.println(text);
+        this.translatedArea.setText(text);
+
+    }//GEN-LAST:event_translate1ActionPerformed
+    public void setMouseEntered_Exited(JButton a, String Imagefile) {
+        ImageIcon icon = new ImageIcon("src/EnglishForDream/" + Imagefile + ".png");
         a.setIcon(icon);
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JCheckBox addvocabtodata;
-    private javax.swing.JTextField addword;
+    private javax.swing.JButton addTextToData;
     private javax.swing.JButton back;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel5;
-    private javax.swing.JButton next;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton texttospeech;
-    private javax.swing.JButton translate;
-    private javax.swing.JLabel translatedLabel;
+    private javax.swing.JButton translate1;
+    private javax.swing.JTextArea translatedArea;
+    private javax.swing.JTextField wordField;
     // End of variables declaration//GEN-END:variables
 }
